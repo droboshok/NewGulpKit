6782cae2e4f1a2920a244d5e0da06b94a733d2b1
droboshok <droboshok@yahoo.com>
2022-08-02 08:31:30 +0300

    file

--- file.sh
+++ file.sh
@@ -5,8 +5,8 @@
 for x in $(cat README.md)
 do
     echo $x
-    printf "/* "$x" ================================================== */" >src/styles/"_"$x".scss" 
-    touch src/html/kit/"_"$x".html"
+    echo "/* "$x" ======================================= */" >src/styles/"_"$x".scss" 
+    echo "<section class=\""$x"\">\n <div class=\"container\">\n\n   </div>\n</section>" > src/html/kit/"_"$x".html"
 
     echo "@@include('_"$x".html') " >> src/html/kit/_main.html
     echo '@import "_'$x'";' >> src/styles/_main.scss
