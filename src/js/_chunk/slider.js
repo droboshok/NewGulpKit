bf7a49e4b3003da3dd9474c01cde85558e38d1e4
droboshok <droboshok@yahoo.com>
2022-08-07 10:11:23 +0300

    slider

---
+++ src/js/_chunk/slider.js
@@ -0,0 +1,47 @@
+const swiper = new Swiper(".slider", {
+  // Optional parameters
+  direction: "horizontal",
+  loop: true,
+  // speed: 400,
+  // spaceBetween: 20,
+  // slidesPerView: 1,
+  // If we need pagination
+  // parallax: true,
+  // pagination: {
+  //   el: ".swiper-pagination",
+  // type: "bullets",
+  //   clickable: true,
+  // },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  // And if we need scrollbar
+  // scrollbar: {
+  //   el: ".swiper-scrollbar",
+  //   draggable: true,
+  // },
+
+  // breakpoints: {
+  //   // when window width is >= 320px
+  //   320: {
+  // slidesPerView: 2,
+  // spaceBetween: 20,
+  //   },
+  //   // when window width is >= 480px
+  //   767: {
+  // slidesPerView: 3,
+  // spaceBetween: 30,
+  //   },
+  //   // when window width is >= 640px
+  //   992: {
+  // slidesPerView: 4,
+  // spaceBetween: 40,
+  //   },
+  // },
+});
+        
+        
