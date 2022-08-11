5feb9316a82d0c38bd359194440d1386bf52eb96
droboshok <droboshok@yahoo.com>
2022-08-11 17:47:32 +0300

    all

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,4 @@
-// @@include("_chunk/burger.js");
-@@include("_chunk/tabs.js")
-// @import "burger";
+ /** @@include("_chunk/burger.js")  */
+ /** @@include("_chunk/tabs.js") */
+ /** @@include("_chunk/slider.js") */
+
